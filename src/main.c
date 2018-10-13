08fd288494b28275478dfa9938098c2649497520
Volodymyr ZOMBER <vzomber@e2r8p9.unit.ua>
2018-10-13 19:29:32 +0300

    .

---
+++ src/main.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vzomber <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/10 14:54:36 by vzomber           #+#    #+#             */
+/*   Updated: 2018/09/10 14:54:37 by vzomber          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+
+int				is_in_way(t_room *room, t_adjlist *way)
+{
+	t_adjlist	*temp;
+
+	temp = way;
+	while (temp)
+	{
+		if (ft_strcmp(temp->room->name, room->name) == 0)
+			return (1);
+		temp = temp->next;
+	}
+	return (0);
+}
+
+int				check_exist(char *line, t_all *tool)
+{
+	t_room		*tmp;
+	int			i;
+
+	CHECK(line);
+	i = 0;
+	while (line[++i] && line[i] != '-')
+		;
+	tmp = tool->rooms;
+	while (tmp)
+	{
+		if (ft_strncmp(tmp->name, line, i) == 0 ||
+			ft_strcmp(tmp->name, &line[i + 1]) == 0)
+			return (1);
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
+void			flags(char **av, t_all *tool)
+{
+	int			i;
+
+	i = 0;
+	while (av[++i])
+	{
+		if (ft_strcmp(av[i], "-l") == 0)
+			print_al(tool);
+		if (ft_strcmp(av[i], "-r") == 0)
+			print_rooms(tool);
+		if (ft_strcmp(av[i], "-w") == 0)
+			show_ways(tool);
+	}
+}
+
+int				main(int ac, char **av)
+{
+	t_all		tool;
+
+	if (ac >= 1)
+	{
+		if (av[1] && ft_strcmp(av[1], "-h") == 0)
+		{
+			ft_printf("usage: ./lem-in [file]  [-w -> shows all ways]\n\t\t"
+					"	[-l -> shows list of links]\n\t\t"
+					"	[-r -> shows list of rooms]\n");
+			exit(1);
+		}
+		ft_bzero(&tool, sizeof(t_all));
+		parse(&tool, 0);
+		algo(&tool);
+		make_ants(tool.ants, &tool);
+		real_ways(&tool);
+		if (tool.qt_ways <= 0 || tool.real_ways <= 0)
+			just_exit("ERROR. NOT ENOUGHT DATA OR NO WAY");
+		go_ants(&tool);
+		flags(av, &tool);
+	}
+	return (0);
+}
---
+++ src/queue.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   queue.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vzomber <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/24 12:01:33 by vzomber           #+#    #+#             */
+/*   Updated: 2018/09/24 12:01:45 by vzomber          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+
+t_adjlist	*get_all_links(t_room *currentvertex, t_all *tool)
+{
+	int		i;
+
+	i = -1;
+	while (tool->al[++i])
+	{
+		if (ft_strcmp(tool->al[i]->room->name, currentvertex->name) == 0)
+			return (tool->al[i]->next);
+	}
+	return (NULL);
+}
+
+void		init_queue(t_all *tool)
+{
+	t_que	*new;
+
+	VCHECK((new = (t_que*)ft_memalloc(sizeof(t_que))));
+	tool->start->visited = 1;
+	tool->start->step = 1;
+	new->room = tool->start;
+	new->next = NULL;
+	tool->q_start = new;
+	tool->q_end = new;
+	tool->q_size = 1;
+}
+
+void		enqueue(t_all *tool, t_adjlist *al, int step)
+{
+	t_que	*new;
+
+	VCHECK((new = (t_que*)ft_memalloc(sizeof(t_que))));
+	new->room = al->room;
+	if (!tool->q_start)
+		tool->q_start = new;
+	if (tool->q_end)
+		tool->q_end->next = new;
+	tool->q_end = new;
+	new->room->visited = 1;
+	new->room->step = step;
+	new->next = NULL;
+	tool->q_size++;
+}
+
+t_room		*dequeue(t_all *tool)
+{
+	t_que	*temp;
+	t_room	*tmp_room;
+
+	temp = tool->q_start;
+	if (temp)
+	{
+		tmp_room = temp->room;
+		tool->q_start = tool->q_start->next;
+		free(temp);
+		tool->q_size--;
+		if (!tool->q_start)
+			tool->q_end = NULL;
+		return (tmp_room);
+	}
+	return (NULL);
+}
